78521dbc785ea324142972be49eb76fbbde5051c
mi-hye <satge13@gmail.com>
2024-07-04 17:25:04 +0900

    Setting: Set cros

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import indexRouter from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors()); // 모든 도메인에서의 요청을 허용
 
 app.use(express.json());
 app.use("/", indexRouter); // indexRouter에 정의된 모든 라우팅을 사용
