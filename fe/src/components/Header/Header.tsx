c71016a8b0680607eedaf3246664b0848426d8e2
mi-hye <satge13@gmail.com>
2024-07-04 10:59:15 +0900

    feat UI: Implement Header Component

---
+++ fe/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+	return (
+		<div className="m-3">
+			<button className="text-base hover:opacity-70 transition">
+				🔐
+			</button>
+		</div>
+	);
+}
+
+export default Header;
