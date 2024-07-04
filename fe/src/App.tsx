48b0f0df1d8290e4c851744cf10c3a29471fea49
mi-hye <satge13@gmail.com>
2024-07-04 10:58:20 +0900

    Setting: Install CRA, tailwind

---
+++ fe/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+
+function App() {
+	return (
+		<div className="w-screen h-screen flex-center">
+			<div className="w-[740px] h-[1000px] bg-slate-200 rounded-lg">
+				<Header />
+			</div>
+		</div>
+	);
+}
+
+export default App;
